db588af1be9eec8ebc0c5aa1cdaf2342e3a2da63
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-04 14:33:07 +0100

    add components main navigation

---
+++ Components/layout/main-navigation.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+// components 
+import Logo from './logo';
+import classes from '../../styles/layout/main-navigation.module.css';
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/posts'>Posts</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
